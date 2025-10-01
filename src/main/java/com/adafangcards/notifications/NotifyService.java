e5e74d64eb738b6f49f9d27d1a8a880bb1b5872b
0xAdafang <adafang@proton.me>
2025-09-30 20:12:46 -0400

    NotifyService

--- src/main/java/com/adafangcards/notifications/NotifyService.java
+++ src/main/java/com/adafangcards/notifications/NotifyService.java
@@ -1,4 +1,16 @@
 package com.adafangcards.notifications;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class NotifyService {
+    private final SimpMessagingTemplate ws;
+
+    public void broadcast(String message) {
+        ws.convertAndSend("/topic/notifcations", message);
+    }
+
 }
