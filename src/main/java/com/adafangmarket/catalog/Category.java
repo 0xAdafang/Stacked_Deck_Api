9f212eacf5258cdb40f633d25ebc4e1e74d39b4e
0xAdafang <adafang@proton.me>
2025-10-07 22:07:42 -0400

    Category

--- src/main/java/com/adafangmarket/catalog/Category.java
+++ src/main/java/com/adafangmarket/catalog/Category.java
@@ -1,4 +1,23 @@
 package com.adafangmarket.catalog;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(indexes = @Index(name="idx_cat_slug", columnList="slug", unique=true))
 public class Category {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @Column(nullable=false) private String name;
+    @Column(nullable=false, unique=true) private String slug;
+    @ManyToOne(fetch = FetchType.LAZY) private Category parent;
+    private Integer position = 0;
 }
