36edc296dfadbdf11b78e19796b2fe5303a23728
0xAdafang <adafang@proton.me>
2025-10-09 20:36:45 -0400

    Inventory

--- src/main/java/com/adafangmarket/catalog/Inventory.java
+++ src/main/java/com/adafangmarket/catalog/Inventory.java
@@ -1,4 +1,35 @@
 package com.adafangmarket.catalog;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Table(indexes = @Index(name="idx_inv_sku", columnList="sku", unique=true))
 public class Inventory {
+    @Id @GeneratedValue private UUID id;
+    @Column(nullable = false, unique = true) private String sku;
+    @Column(nullable = false, unique = true) private Integer quantityAvailaible = 0;
+    @Column(nullable = false) private Integer quantityReserved = 0;
+    private Instant updatedtAt = Instant.now();
+
+    public boolean reserve(int qty) {
+        int available = quantityAvailaible - quantityReserved;
+        if (available >= qty) {
+            quantityReserved =+ qty;
+            updatedtAt = Instant.now();
+            return true;
+
+            } return true;
+    }
+
 }
