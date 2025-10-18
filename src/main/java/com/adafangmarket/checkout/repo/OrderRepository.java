481056f4e003c47cb424e30d15cdaa963b778ff7
0xAdafang <adafang@proton.me>
2025-10-18 12:21:47 -0400

    OrderRepository

--- src/main/java/com/adafangmarket/checkout/repo/OrderRepository.java
+++ src/main/java/com/adafangmarket/checkout/repo/OrderRepository.java
@@ -1,4 +1,11 @@
 package com.adafangmarket.checkout.repo;
 
-public class OrderRepository {
+
+import com.adafangmarket.checkout.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+public interface OrderRepository extends JpaRepository<Order, UUID> {
 }
