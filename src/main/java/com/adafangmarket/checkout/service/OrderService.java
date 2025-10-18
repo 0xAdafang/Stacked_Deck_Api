7ae6fbca9054ff2285dccea0f83355fd02fa09d9
0xAdafang <adafang@proton.me>
2025-10-18 12:52:42 -0400

    CheckoutController

--- src/main/java/com/adafangmarket/checkout/service/OrderService.java
+++ src/main/java/com/adafangmarket/checkout/service/OrderService.java
@@ -1,4 +1,104 @@
 package com.adafangmarket.checkout.service;
 
+import com.adafangmarket.catalog.Inventory;
+import com.adafangmarket.catalog.Product;
+import com.adafangmarket.catalog.repo.InventoryRepository;
+import com.adafangmarket.catalog.repo.ProductRepository;
+import com.adafangmarket.checkout.Cart;
+import com.adafangmarket.checkout.CartItem;
+import com.adafangmarket.checkout.Order;
+import com.adafangmarket.checkout.OrderItem;
+import com.adafangmarket.checkout.dto.CheckoutRequest;
+import com.adafangmarket.checkout.enums.OrderStatus;
+import com.adafangmarket.checkout.repo.CartRepository;
+import com.adafangmarket.checkout.repo.OrderRepository;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final InventoryRepository inventoryRepository;
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    @Transactional
+    public Order createOrder(CheckoutRequest request) {
+        UUID userId = request.userId();
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cart not found"));
+
+        long total = 0L;
+        for(CartItem item : cart.getItems()) {
+            Product product = productRepository.findBySku(item.getSku())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+            Inventory inv = inventoryRepository.findBySku(item.getSku())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Inventory not found"));
+            if (!inv.reserve(item.getQuantity())) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Insufficient stock for " + item.getSku());
+            }
+        }
+
+        String paymentIntentId;
+        try {
+            Stripe.apiKey = stripeApiKey;
+            PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+                    .setAmount(total)
+                    .setCurrency("cad")
+                    .setAutomaticPaymentMethods(PaymentIntentCreateParams.AutomaticPaymentMethods
+                            .builder()
+                            .setEnabled(true)
+                            .build())
+                    .build();
+            PaymentIntent paymentIntent = PaymentIntent.create(params);
+            paymentIntentId = paymentIntent.getId();
+        } catch (StripeException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Payment intent creation failed");
+        }
+
+        Order order = Order.builder()
+                .userId(userId)
+                .totalAmount(total)
+                .stripePaymentIntentId(paymentIntentId)
+                .items(cart.getItems().stream().map(item -> OrderItem.builder()
+                        .order(order)
+                        .sku(item.getSku())
+                        .quantity(item.getQuantity())
+                        .price(item.getPriceAtAdd())
+                        .build()).collect(Collectors.toList()))
+                .build();
+
+        order = orderRepository.save(order);
+        cartRepository.delete(cart);
+
+        return order;
+
+    }
+
+    @Transactional
+    public void handlePaymentWebhook(String paymentIntentId, String status) {
+        Order order = orderRepository.
+                findByStripePaymentIntendId(paymentIntentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found"));
+        order.setStatus(OrderStatus.valueOf(status.toUpperCase()));
+        orderRepository.save(order);
+        }
+    }
+
 }
