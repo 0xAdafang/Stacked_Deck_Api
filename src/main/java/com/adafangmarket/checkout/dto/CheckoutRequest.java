cdd4aec9fa4ff5cc3a6a3248233dc2b59c11ee1d
0xAdafang <adafang@proton.me>
2025-10-18 12:23:17 -0400

    CheckoutRequest

--- src/main/java/com/adafangmarket/checkout/dto/CheckoutRequest.java
+++ src/main/java/com/adafangmarket/checkout/dto/CheckoutRequest.java
@@ -1,4 +1,11 @@
 package com.adafangmarket.checkout.dto;
 
-public class CheckoutRequest {
-}
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record CheckoutRequest (
+         @NotNull(message = "User ID is required")
+         UUID userId
+
+) {}
