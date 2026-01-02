453b5aa05871360e5d24dadbb644dddc0fe71128
0xAdafang <adafang@proton.me>
2026-01-02 10:01:57 -0500

    payment repo

--- src/main/java/com/stackeddeck/payment/repo/PaymentRepository.java
+++ src/main/java/com/stackeddeck/payment/repo/PaymentRepository.java
@@ -1,4 +1,15 @@
 package com.stackeddeck.payment.repo;
 
-public class PaymentRepository {
+import com.stackeddeck.payment.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    Optional<Payment> findByStripeSessionId(String stripeSessionId);
 }
+
